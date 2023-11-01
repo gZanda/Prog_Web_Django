dcab17f269d9043d38d0e7b2207ff598b6d13bcf
gZanda <gabrielfzanda@gmail.com>
2023-11-01 00:31:49 -0300

    Basic Views, URLs and Templates

--- taskies/taskies/settings.py
+++ taskies/taskies/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks_app',
+    'tasks_app', # New app
 ]
 
 MIDDLEWARE = [
--- taskies/tasks_app/views.py
+++ taskies/tasks_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def home(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
+
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
