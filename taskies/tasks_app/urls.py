442acc872bdcc7cfaab3cb6443e87fed28a004f7
gZanda <gabrielfzanda@gmail.com>
2023-11-02 16:25:55 -0300

    Feat: GET by id request

--- taskies/tasks_app/urls.py
+++ taskies/tasks_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('tasks/', views.getTasks, name='tasks'), # GET ALL TASKS
     path('users/', views.getUsers, name='users'), # GET ALL USERS
+    path('tasks/<int:id>/', views.getTaskById, name='taskById'), # GET ONE TASK BY ID
+    path('users/<int:id>/', views.getUserById, name='userById') # GET ONE USER BY ID
 ]
--- taskies/tasks_app/views.py
+++ taskies/tasks_app/views.py
@@ -19,3 +19,23 @@ def getUsers(request):
     users = User.objects.all()                                      
     serializer = UserSerializer(users, many=True)                   
     return Response(serializer.data, status=status.HTTP_200_OK)     
+
+# GET ONE TASK BY ID - Receives the id as a parameter by the URL
+@api_view(['GET'])
+def getTaskById(request, id):
+    try:
+        task = Task.objects.get(id=id)
+        serializer = TaskSerializer(task, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+# GET ONE USER BY ID - Receives the id as a parameter by the URL
+@api_view(['GET'])
+def getUserById(request, id):
+    try:
+        user = User.objects.get(id=id)
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
