80d7fa228cfc9534bf5cb7ae2f81595b7a858be5
gZanda <gabrielfzanda@gmail.com>
2023-11-08 18:21:36 -0300

    Fix: better urls name & admin.txt

--- taskies/tasks_app/urls.py
+++ taskies/tasks_app/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
     path('users/', views.getUsers, name='users'), # GET ALL USERS
     path('tasks/<int:id>/', views.getTaskById, name='taskById'), # GET TASK 
     path('users/<int:id>/', views.getUserById, name='userById'), # GET USER 
-    path('postTask/', views.postTask, name='postTask'), # POST TASK
-    path('postUser/', views.postUser, name='postUser'), # POST USER
+    path('createTask/', views.postTask, name='postTask'), # POST TASK
+    path('createUser/', views.postUser, name='postUser'), # POST USER
     path('deleteTask/<int:id>/', views.deleteTaskById, name='deleteTaskById'), # DELETE TASK    
     path('deleteUser/<int:id>/', views.deleteUserById, name='deleteUserById'), # DELETE USER 
-    path('putTask/<int:id>/', views.putTaskById, name='putTaskById'), # UPDATE TASK 
-    path('putUser/<int:id>/', views.putUserById, name='putUserById'), # UPDATE USER 
+    path('editTask/<int:id>/', views.putTaskById, name='putTaskById'), # UPDATE TASK 
+    path('editUser/<int:id>/', views.putUserById, name='putUserById'), # UPDATE USER 
     path('betterCreateUser/', views.userRegistration, name='userRegistration'), # BETTER CREATE USER
     path('betterUserLogin/', views.userLogin, name='userLogin'), # BETTER USER LOGIN
 ]
